082b902e267b3cdd74323a39dbd8614135933010
Billfred <github@billfredindustries.com>
2021-07-31 16:42:06 -0400

    Oh hey, we have climbers.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,8 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Timer;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -19,6 +21,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 public class Robot extends TimedRobot {
   private DifferentialDrive m_myRobot;
   private XboxController m_driverController = new XboxController(0);
+  private XboxController m_gunnerController = new XboxController(1);
   private static final int leftDeviceID = 1; 
   private static final int leftFollowID = 2;
   private static final int rightDeviceID = 3;
@@ -36,6 +39,11 @@ public class Robot extends TimedRobot {
   private static final int dtCurrentLimit = 80; //per-motor current limit for the robot; 80A tends to be safe on carpet unless the battery is too weak
   private static final int accelCurrentLimit = 20; //current limiting for the two accelerator rollers
   private static final double bumperTurnRate = 0.5; //smaller numbers mean more aggressive turning
+  private final Timer m_timer = new Timer();
+
+  // And climbers.
+  Spark leftClimber = new Spark(0);
+  Spark rightClimber = new Spark(1);
 
   @Override
   public void robotInit() {
@@ -61,9 +69,9 @@ public class Robot extends TimedRobot {
     m_feeder = new CANSparkMax(feederID, MotorType.kBrushed);
 
     /**
-     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
-     * in the SPARK MAX to their factory default state. If no argument is passed, these
-     * parameters will not persist between power cycles
+     * The RestoreFactoryDefaults method ensures settings are cleared. This is important if we stuff a
+     * new SPARK MAX controller into the robot in an emergency, especially if we bum one off another team.
+     * This way, everything is set in code and you only have to set the CAN ID properly.
      */
     m_leftMotor.restoreFactoryDefaults();
     m_rightMotor.restoreFactoryDefaults();
@@ -73,13 +81,13 @@ public class Robot extends TimedRobot {
     m_accelerator.restoreFactoryDefaults();
     m_feeder.restoreFactoryDefaults();
 
-    // Two of our drivetrain motors act as followers of the other one on the same side, let's make that official.
+    // Two of our drivetrain motors act as followers of the other one on the same side, let's make it official.
     // You'll notice we almost never talk about them again; that's intentional.
 
     m_leftFollow.follow(m_leftMotor);
     m_rightFollow.follow(m_rightMotor);
 
-    // Now, let's set current limiting on our motors.
+    // Now, let's set current limiting on our motors so the robot doesn't brown out when launching.
     m_leftMotor.setSmartCurrentLimit(dtCurrentLimit);
     m_leftFollow.setSmartCurrentLimit(dtCurrentLimit);
     m_rightMotor.setSmartCurrentLimit(dtCurrentLimit);
@@ -89,36 +97,37 @@ public class Robot extends TimedRobot {
     // Oh, and we have to create the DifferentialDrive for our drivetrain too. Done.
     m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
 
+
   }
 
   @Override
   public void teleopPeriodic() {
-
-    // Let's do quick turns with the bumpers of the driver's controller!
-
-    while (m_driverController.getBumper(Hand.kLeft)) {
-      m_leftMotor.set(-m_driverController.getY(Hand.kLeft)*bumperTurnRate);
-      m_rightMotor.set(m_driverController.getY(Hand.kLeft));
-    }
-    
-    while (m_driverController.getBumper(Hand.kRight)) {
-      m_leftMotor.set(-m_driverController.getY(Hand.kLeft));
-      m_rightMotor.set(m_driverController.getY(Hand.kRight)*bumperTurnRate);
-    }
-
-    // Oh, we don't have bumpers pressed? Drive with split arcade drive.
+    // Now let's drive with split arcade drive.
     // That means that the Y axis of the left stick moves forward
     // and backward, and the X of the right stick turns left and right.
-    while (!m_driverController.getBumper(Hand.kLeft) && !m_driverController.getBumper(Hand.kRight))
+    // while (!m_driverController.getBumper(Hand.kLeft) && !m_driverController.getBumper(Hand.kRight)) {
     m_myRobot.arcadeDrive(
-        m_driverController.getY(Hand.kLeft)*-1, m_driverController.getX(Hand.kRight)*.8);
-    
+    m_driverController.getY(Hand.kLeft)*-1, m_driverController.getX(Hand.kRight)*.8);
+    // } 
     
     // This next section controlls the ball accelerator, feeder roller, and shooter.
     // You'll notice this is all set off the two Xbox triggers.  It could be better.
     // I hope you'll make this into a couple PID loops down the road.
-    m_shooter.set(1.5*(m_driverController.getTriggerAxis(Hand.kLeft)));
-    m_accelerator.set(-1*(m_driverController.getTriggerAxis(Hand.kLeft)));
-    m_feeder.set(m_driverController.getTriggerAxis(Hand.kRight));
+
+    m_shooter.set(1.5*(m_gunnerController.getTriggerAxis(Hand.kLeft)));
+    m_accelerator.set(-1*(m_gunnerController.getTriggerAxis(Hand.kLeft)));
+    m_feeder.set(m_gunnerController.getTriggerAxis(Hand.kRight));
+
+    // Climber action goes here.  Holding both driver bumpers runs the motors.
+    // WARNING: Direction and speed of the climbers is untested.
+
+    if (m_driverController.getBumper(Hand.kLeft) && m_driverController.getBumper(Hand.kRight)) {
+    // NO REALLY, THIS SPEED AND DIRECTION IS UNTESTED ON LORI.
+      leftClimber.set(0.75);
+      rightClimber.set(0.75);
+    } else {
+      leftClimber.set(0);
+      rightClimber.set(0);
+    }
   }
 }
